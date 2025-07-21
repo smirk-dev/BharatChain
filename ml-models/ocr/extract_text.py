ae1631839c2af95cd4347cbe84d41b2ae0a60338
Suryansh Mishra <suryansh.mishra.sm6@gmail.com>
2025-07-21 15:46:47 +0530

    line change

--- ml-models/ocr/extract_text.py
+++ ml-models/ocr/extract_text.py
@@ -1,7 +1,6 @@
 # OCR Extraction Script using pytesseract
 from PIL import Image
 import pytesseract
-
 def extract_text(image_path):
     return pytesseract.image_to_string(Image.open(image_path))
 
