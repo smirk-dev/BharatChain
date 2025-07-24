ac74e872196c2ef56842f5d59c4bf54ea848dc44
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-07-24 18:11:04 +0530

    add jest configuration file

---
+++ server/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'routes/**/*.js',
+    'services/**/*.js',
+    'models/**/*.js',
+    'middleware/**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testTimeout: 30000,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  verbose: true
+};
