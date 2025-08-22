2cc4bd4c1bbc182e63fa0cdacfea9e00f4479410
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-22 20:06:18 +0530

    fix: Update setupFilesAfterEnv path in jest.config.js

--- server/jest.config.js
+++ server/jest.config.js
@@ -12,6 +12,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   testTimeout: 30000,
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/../tests/setup.js'],
   verbose: true
 };
