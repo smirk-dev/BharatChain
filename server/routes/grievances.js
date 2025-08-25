e91f82d0e4f3307fe59898df613338ef4556b61b
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-08-25 21:46:53 +0530

    style: Import User and Grievance models in grievances route

--- server/routes/grievances.js
+++ server/routes/grievances.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const { User, Grievance } = require('../models');
 
 /**
  * @route GET /api/grievances
