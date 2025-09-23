f5d88fbb1d4339a4b715fabae1030bfaaae7b4d4
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-09-23 19:21:12 +0530

    fix: Update localhost URL in hardhat configuration for consistency

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://localhost:8545"
     },
     mumbai: {
       url: process.env.MUMBAI_RPC_URL || "https://rpc-mumbai.maticvigil.com",
